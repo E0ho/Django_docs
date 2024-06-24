cb63f92e11996ea54838e7f7f598cc36eea2dc85
E0ho <bbchoi6@hufs.ac.kr>
2024-06-24 15:47:44 +0900

    docs: Admin 모드 Model 추가

--- blog/admin.py
+++ blog/admin.py
@@ -11,4 +11,22 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ('title', 'body')       # 조회 기능
 
 
+# 게시글
 admin.site.register(Post, PostAdmin)
+
+# 댓글 (게시글 참조)
+admin.site.register(Comment)
+
+
+
+# 유저 정보
+admin.site.register(User)
+
+# Profile (유저 참조)
+admin.site.register(Profile)
+
+
+
+
+# Profile (유저 참조)
+admin.site.register(Tag)
