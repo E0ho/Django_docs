63ac32e2ac4e64077f3e3ebf5a215b2143742774
E0ho <bbchoi6@hufs.ac.kr>
2024-06-24 12:59:48 +0900

    feat: URL 변수를 활용한 Detail 함수

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('/', views.index)
+    path('', views.index),
+    path('<int:id>', views.detail)       # URL 변수
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def index():
+def index(request):
     return HttpResponse('ok')
+
+
+# URL 변수 활용
+def detail(request, id):
+    return HttpResponse(id)
