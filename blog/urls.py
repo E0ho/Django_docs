57868f8a654e560963becb602283ec741fbc629c
E0ho <bbchoi6@hufs.ac.kr>
2024-06-24 17:21:20 +0900

    feat: 사용자 Profile 조회

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:id>', views.detail, name='detail')       # URL 변수
-
+    path('', views.index, name='index'),                 # 메인 페이지 (목록)
+    path('<int:id>/', views.detail, name='detail'),       # 상세 페이지
+    path('profile/', views.profile, name='profile')      # 프로필 페이지
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, HttpResponse
 from .models import *
-# Create your views here.
 
+
+# 메인 페이지 처리 함수 (게시판 목록)
 def index(request):
     posts = Post.objects.all()
 
@@ -10,9 +11,31 @@ def index(request):
     return render(request, 'blog/list.html', context)
 
 
-# URL 변수 활용
+
+# 상세 페이지 처리 함수
 def detail(request, id):
     post = get_object_or_404(Post, id = id)
+    comments = post.comments.all()
 
-    context = {'post' : post}
+    context = {
+        'post' : post,
+        'comments' : comments
+        }
+    
     return render(request, 'blog/detail.html', context)
+
+
+
+# 프로필 처리 함수
+def profile(request):
+
+    # 임시 테스트용
+    user = User.objects.first()     # 사용자
+    profile = user.profile          # 사용자 매칭 프로필
+    print(profile.phone_number)
+    context = {
+        'user' : user,
+        'profile':profile
+        }
+
+    return render(request, 'blog/profile.html', context)
