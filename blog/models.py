cb774046c481f408cd31a426ebe0ecc75182b772
E0ho <bbchoi6@hufs.ac.kr>
2024-06-24 15:17:29 +0900

    feat: 1:N 관계 Model 참조 (ForeignKey)

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,13 @@ class Post(models.Model):
   # Model Instance 대표 출력 값
   def __str__(self):
     return self.title
+  
+
+# 1:N 관계 (하나의 Post -> 여러 Comment)
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')  # 외래 참조 키
+    author = models.CharField(max_length=20)
+    message = models.TextField()
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    deleted = models.BooleanField(default=False)
