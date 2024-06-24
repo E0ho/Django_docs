f4389331d445b542fb597cfcf8f50ab6da737253
E0ho <bbchoi6@hufs.ac.kr>
2024-06-24 14:55:25 +0900

    feat: Post Model 생성

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "chinook.db",             # DB 경로 변경
+        "NAME": BASE_DIR / "db.sqlite3",             # DB 경로 변경
     }
 }
 
