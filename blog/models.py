9590829edd3cb0619bf6f510fc9792d3fe6a5ea4
E0ho <bbchoi6@hufs.ac.kr>
2024-06-25 13:18:05 +0900

    feat: 게시글 Model URL Reverse

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 class Post(models.Model):
@@ -10,6 +12,11 @@ class Post(models.Model):
   def __str__(self):
     return self.title
   
+  # Model Return URL
+  def get_absolute_url(self):
+    return reverse("blog:detail", kwargs={"id": self.id})
+
+
 
 
 
