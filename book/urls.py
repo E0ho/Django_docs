017c049a883743517e15dbf06b822a1af471d0e6
E0ho <bbchoi6@hufs.ac.kr>
2024-06-26 11:45:46 +0900

    feat: GenericView 기반 데이터 추가

--- book/urls.py
+++ book/urls.py
@@ -8,7 +8,7 @@ app_name = 'book'
 urlpatterns = [
     path('', ListView.as_view(model=Book), name='list'),
     path('detail/<pk>/', DetailView.as_view(model=Book), name='detail'),
-    path('create/', DetailView.as_view(model=Book), name='create'),
+    path('create/', CreateView.as_view(model=Book, fields='__all__'), name='create'),
     path('update/<pk>/', DetailView.as_view(model=Book), name='update'),
     path('delete/<pk>/', DetailView.as_view(model=Book), name='delete'),
 ]
