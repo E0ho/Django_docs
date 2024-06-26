06944c5c68aac22617b8fc569c16d57a8b00adeb
E0ho <bbchoi6@hufs.ac.kr>
2024-06-26 11:47:47 +0900

    feat: GenericView 데이터 수정

--- book/urls.py
+++ book/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('', ListView.as_view(model=Book), name='list'),
     path('detail/<pk>/', DetailView.as_view(model=Book), name='detail'),
     path('create/', CreateView.as_view(model=Book, fields='__all__'), name='create'),
-    path('update/<pk>/', DetailView.as_view(model=Book), name='update'),
-    path('delete/<pk>/', DetailView.as_view(model=Book), name='delete'),
+    path('update/<pk>/', UpdateView.as_view(model=Book, fields='__all__'), name='update'),
+
 ]
