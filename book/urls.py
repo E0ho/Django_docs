e599eb5e00d8aeb6f97eb06b16d519c09b3267da
E0ho <bbchoi6@hufs.ac.kr>
2024-06-26 13:47:08 +0900

    feat: GenericView 데이터 삭제

--- book/urls.py
+++ book/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.views.generic import *
 from . import views
 from .models import *
@@ -10,5 +10,5 @@ urlpatterns = [
     path('detail/<pk>/', DetailView.as_view(model=Book), name='detail'),
     path('create/', CreateView.as_view(model=Book, fields='__all__'), name='create'),
     path('update/<pk>/', UpdateView.as_view(model=Book, fields='__all__'), name='update'),
-
+    path('delete/<pk>/', DeleteView.as_view(model=Book, success_url=reverse_lazy('book:list')), name='delete'),
 ]
